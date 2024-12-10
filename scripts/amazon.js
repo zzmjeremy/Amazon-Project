261d6b49bda1486ab3bcceb9652988edd0fd55da
ZZM <zhuominzhucontact@gmail.com>
2024-12-10 14:40:26 -0800

    Convert products into classes

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -20,16 +20,16 @@ products.forEach((product) => {
           <div class="product-rating-container">
             <img
               class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars * 10}.png"
+              src="${product.getStarsUrl()}"
             />
             <div class="product-rating-count link-primary">${
               product.rating.count
             }</div>
           </div>
 
-          <div class="product-price">$${formatCurrency(
-            product.priceCents
-          )}</div>
+          <div class="product-price">
+          ${product.getPrice()}
+          </div>
           <!-- have 2 decimals -->
 
           <div class="product-quantity-container">
